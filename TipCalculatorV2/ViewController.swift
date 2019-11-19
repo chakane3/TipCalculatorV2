758b287ed84055ff20e2b41f7b8ec49f4c614c94
Chakane Shegog <chakanezshegog@gmail.com>
2019-11-18 22:48:54 -0500

    Initial Commit

---
+++ TipCalculatorV2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculatorV2
+//
+//  Created by Chakane Shegog on 11/18/19.
+//  Copyright © 2019 Chakane Shegog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
